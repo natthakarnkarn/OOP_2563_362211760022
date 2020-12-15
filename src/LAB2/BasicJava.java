085cc23073a1c3cfe89b4ec4b62020583b00c1a1
natthakarnkarn <52774783+natthakarnkarn@users.noreply.github.com>
2020-12-15 10:52:29 +0700

    Update BasicJava.java

--- src/LAB2/BasicJava.java
+++ src/LAB2/BasicJava.java
@@ -5,7 +5,7 @@ public class BasicJava {
     public static void main(String[] args) {
 
         System.out.println("Data type in Programming.");
-        //Integer จำนวนเต็ม เช่น -100 -10 -2 -1 0 1 2 3 10 100
+        //Integer จำนวนเต็ม เช่น -100 -10 -2 -1 0 1 2 3 10 100 
         int number;
         number = 100; //ตัวแปล number เก็บค่าข้อมูลจำนวนเต็ม100
 
