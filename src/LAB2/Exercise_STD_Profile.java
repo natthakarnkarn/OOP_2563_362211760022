1747b4372d12f3836107624e9e01552c6f285f31
Win10x64 <natthakarnkarn29@gmail.com>
2021-01-05 09:09:15 +0700

    Bas

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,32 @@
+package LAB2;
+
+public class Exercise_STD_Profile {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter an name: ");
+        String name = scanner.next();
+
+
+        System.out.println("Enter an lastname: ");
+        String lastname = scanner.next();
+
+
+        System.out.println("Enter an id: ");
+        String std_id = scanner.next();
+
+
+        System.out.println("Enter an age: ");
+        int age = scanner.nextInt();
+
+
+        System.out.println("Enter an email: ");
+        String email = scanner.next();
+
+
+
+
+    }
+}
